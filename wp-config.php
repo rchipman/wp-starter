dd7804c51a0095596c011b158ca2530c41b4676f
Rob Chipman <jrchipman1@gmail.com>
2014-04-01 00:40:23 -0400

    Shortened template name, added salts to template and removed from config

--- wp-config.php
+++ wp-config.php
@@ -61,24 +61,6 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'NEW SALTS');
-define('SECURE_AUTH_KEY',  'NEW SALTS');
-define('LOGGED_IN_KEY',    'NEW SALTS');
-define('NONCE_KEY',        'NEW SALTS');
-define('AUTH_SALT',        'NEW SALTS');
-define('SECURE_AUTH_SALT', 'NEW SALTS');
-define('LOGGED_IN_SALT',   'NEW SALTS');
-define('NONCE_SALT',       'NEW SALTS');
-
 /**#@-*/
 
 /**
---
+++ wp-cvt.php
@@ -0,0 +1,27 @@
+<?php
+
+/* CLIENT VARIABLE */
+define('WP_CLIENT',         '');
+
+/* DEVELOPMENT VARIABLES */
+define('DEV_DB_USER',       '');
+define('DEV_DB_PASS',       '');
+define('DEV_URL',           '');
+
+/* PRODUCTION VARIABLES */
+define('PROD_DB_USER',      '');
+define('PROD_DB_PASS',      '');
+define('PROD_URL',          '');
+
+/* 
+* SALTS 
+* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+*/
+define('AUTH_KEY',          '');
+define('SECURE_AUTH_KEY',   '');
+define('LOGGED_IN_KEY',     '');
+define('NONCE_KEY',         '');
+define('AUTH_SALT',         '');
+define('SECURE_AUTH_SALT',  '');
+define('LOGGED_IN_SALT',    '');
+define('NONCE_SALT',        '');
